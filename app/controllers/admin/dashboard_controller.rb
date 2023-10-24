ccb1d4f6e6d7d526aacc1a909806fec776b04815
Rina Inada <indrn@icloud.com>
2023-10-24 00:16:12 +0000

    Implement admin dashboard controller and view

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,15 @@
 class Admin::DashboardController < ApplicationController
+
+  # admin authentification, name and password are set in .env
+  http_basic_authenticate_with name: ENV["ADMIN_NAME"], password: ENV["ADMIN_PASSWORD"]
+
+  # method to retrive data from the database
+  # will pass the data to admin/dashboard/show.html/erb
   def show
+    # set all the products to @products variable
+    @products = Product.all
+
+    # set all the categories to @categories valiable
+    @categories = Category.all
   end
 end

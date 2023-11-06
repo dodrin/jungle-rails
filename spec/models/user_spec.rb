1a06e07b0725c254b14a0aa7171fe5ff42368569
Rina Inada <indrn@icloud.com>
2023-11-06 18:57:04 +0000

    Add user spec

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,89 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'Validation' do
+
+    it 'should create a valid user' do
+      user = User.create(
+        first_name: 'Kuma',
+        last_name: 'Kumatest',
+        email: 'test@test.com',
+        password: 'test123',
+        password_confirmation: 'test123'
+      )
+      expect(user).to be_valid
+    end
+
+    it 'should have matching password and password_confirmation' do
+      user = User.create(
+        first_name: 'Kuma',
+        last_name: 'Kumatest',
+        email: 'test@test.com',
+        password: 'test123',
+        password_confirmation: 'test345'
+      )
+      expect(user).not_to be_valid
+    end
+
+    it 'is not valid if the email is not unique' do
+      user1 = User.create(
+        first_name: 'Kuma',
+        last_name: 'Kumatest',
+        email: 'test@test.com',
+        password: 'test123',
+        password_confirmation: 'test345'
+      )
+
+      user2 = User.create(
+        first_name: 'Tanuki',
+        last_name: 'Araikuma',
+        email: 'TEST@TEST.com',
+        password: 'test123',
+        password_confirmation: 'test345'
+      )
+      expect(user2).to_not be_valid
+    end
+
+    it 'should have email' do
+      user = User.create(
+        first_name: 'Kuma',
+        last_name: 'Kumatest',
+        password: 'test123',
+        password_confirmation: 'test345'
+      )
+      expect(user).to_not be_valid
+    end
+
+    it 'should have first name' do
+      user = User.create(
+        last_name: 'Kumatest',
+        email: 'test@test.com',
+        password: 'test123',
+        password_confirmation: 'test345'
+      )
+      expect(user).to_not be_valid    
+    end
+    
+    it 'should have last name' do
+      user = User.create(
+        first_name: 'Kuma',
+        email: 'test@test.com',
+        password: 'test123',
+        password_confirmation: 'test345'
+      )
+      expect(user).to_not be_valid    
+    end
+
+    it 'should have password' do
+      user = User.create(
+        first_name: 'Kuma',
+        last_name: 'test',
+        email: 'test@test.com',
+        password_confirmation: 'test345'
+      )
+      expect(user).to_not be_valid   
+    end
+
+
+  end
+end
